9ce9a7f879038b60225f2b5d77af61057260dda5
andci <andreas.cisnevik@mindroad.se>
2019-01-10 14:43:27 +0100

    First firebase example

--- stgreact/src/fire.js
+++ stgreact/src/fire.js
@@ -3,6 +3,7 @@ var config = {
   apiKey: "AIzaSyBbkved5B3UkHxKrJcmnsWyK00uPVCnkeE",
   authDomain: "stg-horse-organizer.firebaseapp.com",
   databaseURL: "https://stg-horse-organizer.firebaseapp.com",
+  projectId: "stg-horse-organizer",
   storageBucket: "stg-horse-organizer.appspot.com",
   messagingSenderId: "63411012494"
 };
