3815b5a1bb7cb69f7bbf0e95bdfffa50d32c8cb0
andci <andreas.cisnevik@mindroad.se>
2019-01-15 11:49:22 +0100

    Working react-firebase web page

---
+++ src/config/prod.js
@@ -0,0 +1,5 @@
+export const FirebaseConfig = {
+    apiKey: "AIzaSyBbkved5B3UkHxKrJcmnsWyK00uPVCnkeE",
+    authDomain: "stg-horse-organizer.firebaseapp.com",
+    databaseURL: "https://stg-horse-organizer.firebaseio.com"
+  };
